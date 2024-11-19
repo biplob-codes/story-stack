28c54defe5bdfb8458eafaeb5aa990a3f7137a9a
Biplob Talukdar <biplob.codes@gmail.com>
2024-11-19 19:08:24 +0600

    Initialize db along with connection pooling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET_KEY: string;
+  };
+}>();
+// const prisma = new PrismaClient({datasourceUrl:env.DATABASE_URL}).$extends(withAccelerate());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+export default app;
